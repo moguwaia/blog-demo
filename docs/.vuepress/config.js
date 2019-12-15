dda50c71945a89ff2ae0bcefb6da4a3ef209b07f
ysmartboyk <17362953807@163.com>
2019-12-15 15:08:14 +0800

    增加

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -6,7 +6,7 @@ module.exports = {
 module.exports = {
     themeConfig: {
         // 你的GitHub仓库，请正确填写
-        repo: 'https://github.com/ysmartboyk/moguwai.github.io/blog-demo',
+        repo: 'https://github.com/ysmartboyk/moguwai.github.io/cnblogs',
         // 自定义仓库链接文字。
         repoLabel: 'My GitHub',
         nav: [
